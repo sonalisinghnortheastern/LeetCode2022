2ff2b8ce5e0119aa6770b0f7db327eb08270da8b
Sonali Singh <90349708+sonalisinghnortheastern@users.noreply.github.com>
2022-12-23 22:58:01 -0500

    Added two new ways of finding lexographically greater string

--- LexicographicalMaxString.java
+++ LexicographicalMaxString.java
@@ -19,3 +19,47 @@ public static String lexographicString(String str){
         }
         return maxValue;
     }
+
+
+public static String lexographicString(String str){
+       String mx = "";
+        for (int i = 0; i < str.length(); ++i) {
+            if (mx.compareTo(str.substring(i)) <= 0) {
+                mx = str.substring(i);
+            }
+        }
+ 
+        return mx;
+    }
+
+static String LexicographicalMaxString(String str)
+  {
+    char maxchar = 'a';
+    ArrayList<Integer> index = new ArrayList<Integer>();
+ 
+    // We store all the indexes of maximum
+    // characters we have in the string
+    for (int i = 0; i < str.length(); i++)
+    {
+      if (str.charAt(i) >= maxchar)
+      {
+        maxchar = str.charAt(i);
+        index.add(i);
+      }
+    }
+    String maxstring = "";
+ 
+    // We form a substring from that maximum
+    // character index till end and check if
+    // its greater that maxstring
+    for (int i = 0; i < index.size(); i++)
+    {
+      if (str.substring(index.get(i),
+                        str.length()).compareTo( maxstring) > 0)
+      {
+        maxstring = str.substring(index.get(i),
+                                  str.length());
+      }
+    }
+    return maxstring;
+  }
