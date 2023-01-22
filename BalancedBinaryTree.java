bb2acc4a14d9c9969fc3a34bdae99a3e7dbaec36
Sonali Singh <90349708+sonalisinghnortheastern@users.noreply.github.com>
2023-01-22 14:24:46 -0500

    Added Balanced Binary Tree

---
+++ BalancedBinaryTree.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+       return isBalancedHelper(root)!=-1;
+    }
+    private int isBalancedHelper(TreeNode root){
+        if(root==null) return 0;
+        int left=isBalancedHelper(root.left);
+        int right=isBalancedHelper(root.right);
+        if(left==-1 || right==-1 ||Math.abs(left-right)>1) return -1;
+        return Math.max(left,right)+1;
+    }
+}
